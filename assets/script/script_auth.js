dc254c70f4b58db169776af12e3637c710c1c85e
niraj-2022 <116768785+niraj-2022@users.noreply.github.com>
2025-03-01 20:47:06 +0530

    Add files via upload

---
+++ assets/script/script_auth.js
@@ -0,0 +1,18 @@
+function signup_animate(){
+    document.getElementById("login_1").style.display="none";
+    document.getElementById("signup_1").style.display="block";
+    document.getElementById("TG_login").style.backgroundColor="#fff";
+    document.getElementById("TG_login").style.color="#000";
+    document.getElementById("TG_signup").style.backgroundColor="#611fd3";
+    document.getElementById("TG_signup").style.color="#fff";
+ }
+ 
+function login_animate(){
+    document.getElementById("signup_1").style.display="none";
+    document.getElementById("login_1").style.display="block";
+    document.getElementById("TG_login").style.backgroundColor="#611fd3";
+    document.getElementById("TG_login").style.color="#fff";
+    document.getElementById("TG_signup").style.backgroundColor="#fff";
+    document.getElementById("TG_signup").style.color="#000";
+ }
+ 
